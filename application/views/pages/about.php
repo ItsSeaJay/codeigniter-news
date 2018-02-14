4e5425be275635a9c69f01df8388b711586bc9b5
Unknown <b6010572@my.shu.ac.uk>
2018-02-14 09:55:30 +0000

    Added home and about pages

---
+++ application/views/pages/about.php
@@ -0,0 +1,5 @@
+<p>
+  CodeIgniter News is a completely fictional organisation comitted to the
+  virulent spread of misinformation through social memes, filtered through the
+  finest ignorance and intolerance only a lack of practical education can buy.
+</p>
